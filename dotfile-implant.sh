c89767851effb649c36fd8f3cef6fd7f7d991e3f
Xavier Lopez <xavier@zavestudios.com>
2020-03-02 10:47:59 -0800

    refactored the shell script

--- dotfile-implant.sh
+++ dotfile-implant.sh
@@ -2,27 +2,13 @@
 # Discover which unix/linux distribution we're on
 # Purge the default version of vim, along with the configuration files, if they're installed.
 # Install vim, tmux
-# Remove config files for vim and tmux, if they're there
-# Copy .vimrc and .tmux.conf from zave's github
+# Copy the new vim and tmux config files from zave's github
 #
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # mac TESTED PASSED
-    echo "Hello from darwin"
-    brew update
-elif [[ -f /etc/lsb-release ]]; then
+    brew update && brew install vim tmux
+elif [[ -f /etc/lsb-release ]] || [[ -f /etc/debian_version ]] || [[ -f /etc/SUSE-brand ]]; then
     # For some versions of Debian/Ubuntu without lsb_release command TESTED PASSED
-    sudo apt update
-    sudo apt-get purge vim -y
-    sudo apt-get install vim curl tmux -y
-    rm -f ~/.tmux.conf ~/.vimrc
-elif [[ -f /etc/debian_version ]]; then
-    # Older Debian/Ubuntu/etc. TESTED PASSED
-    sudo apt update
-    sudo apt-get purge vim -y
-    sudo apt-get install vim curl tmux -y
-    rm -f ~/.tmux.conf ~/.vimrc
-elif [[ -f /etc/SUSE-brand ]]; then
-    # openSUSE TESTED PASSED
     sudo apt update
     sudo apt-get purge vim -y
     sudo apt-get install vim curl tmux -y
@@ -35,10 +21,10 @@ elif [[ -f /etc/redhat-release ]]; then
     rm -f ~/.tmux.conf ~/.vimrc
 elif [[ -f /etc/freebsd-update.conf ]]; then
     # Freebsd TESTED FAILED
-    echo "Sorry, this script won't work on this linux distribution.""
+    echo "Sorry, this script won't work on this unix / linux distribution."
 else
     # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
-    echo "Hello from unknown"
+     echo "Sorry, I don't recognive this unix / linux distribution."
 fi
 # Copy .vimrc and .tmux.conf from zave's github
 curl https://raw.githubusercontent.com/zave/dotfiles/master/.vimrc > ~/.vimrc
